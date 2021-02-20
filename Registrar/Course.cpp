3f4913b83329e36520d8e805f6f505bfbfc98c40
wko1375 <38301560+wko1375@users.noreply.github.com>
2021-02-20 00:24:38 -0500

    Update Course.cpp

--- Registrar/Course.cpp
+++ Registrar/Course.cpp
@@ -1,35 +1,32 @@
 //
 // Created by William on 3/9/2018.
 //
-
-#ifndef COURSE_H
-#define COURSE_H
-
 #include "Student.h"
+#include "Course.h"
 #include <string>
-#include <vector>
-
+#include <Vector>
+using namespace std;
 
 namespace BrooklynPoly {
-
-    class Student;
-    class Course{
-    public:
-        Course(const std::string& courseName);
-        int getSize()const;
-        const std::string& getName()const;
-        Student getStudent(int index)const;
-        void deleteStudentptr(int index);
-        const std::string& getStudentName(int index)const;
-        void addStudent(Student& student);
-    private:
-        std::vector<Student*> students;
-        std::string name;
-    };
-
+    Course::Course(const string& courseName) : name(courseName){}
+    int Course::getSize()const{
+        return students.size();
+    }
+    const string& Course::getName()const{
+        return name;
+    }
+    Student Course::getStudent(int index)const{
+        return *students[index];
+    }
+    void Course::deleteStudentptr(int index){
+        students[index], students[students.size()] = students[students.size()], students[index];
+        students.pop_back();
+    }
+    const string& Course::getStudentName(int index)const{
+        return students[index] -> getName();
+    }
+    void Course::addStudent(Student& student){
+        Student* sptr = &student;
+        students.push_back(sptr);
+    }
 }
-
-
-
-
-#endif //UNTITLED1_COURSE_H
